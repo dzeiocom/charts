789d83d7f1b496218657398d42a67965c95650d5
Avior <f.bouillon@aptatio.com>
2023-02-08 17:10:47 +0100

    fix: crash while click
    Signed-off-by: Avior <f.bouillon@aptatio.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,16 +4,13 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.4.0")
-
-        // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle files
+        classpath("com.android.tools.build:gradle:7.4.1")
     }
 }
 
 plugins {
-    id("com.android.application") version "7.4.0" apply false
-    id("com.android.library") version "7.4.0" apply false
+    id("com.android.application") version "7.4.1" apply false
+    id("com.android.library") version "7.4.1" apply false
     id("org.jetbrains.kotlin.android") version "1.7.0" apply false
 }
 
